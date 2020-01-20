12df1aa18b27c78b8989d5c28db492b9e7aca30b
Burak <brkcrdk@gmail.com>
2020-01-20 21:35:37 +0200

    typeDefs added

--- api/functions/graphql/schema.js
+++ api/functions/graphql/schema.js
@@ -1,9 +1,48 @@
 const { gql } = require("apollo-server-express");
 
 const schema = gql`
+  type Persons {
+    id: Int
+    name: String
+  }
+
+  type Person {
+    id: Int
+    name: String
+    username: String
+    email: String
+    address: Address
+    phone: String
+    website: String
+    company: Company
+    posts: [Post]
+  }
+  type Address {
+    street: String
+    suite: String
+    city: String
+    zipcode: String
+  }
+  type Company {
+    name: String
+    catchPhrase: String
+  }
+  type Post {
+    id: Int
+    title: String
+    body: String
+    comments: [Comment]
+  }
+  type Comment {
+    id: Int
+    name: String
+    email: String
+    body: String
+  }
   type Query {
-    "A simple type for getting started!"
-    hello: String
+    persons: [Persons]
+    person(userId: Int!): Person
+    post(postId: Int!): Post
   }
 `;
 
