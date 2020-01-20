93cfd134b7b8e2094fef95cf36b7995923c63e85
Burak <brkcrdk@gmail.com>
2020-01-20 21:42:36 +0200

    Resolvers added

--- api/functions/graphql/resolver.js
+++ api/functions/graphql/resolver.js
@@ -1,6 +1,31 @@
+const mainUrl = `https://jsonplaceholder.typicode.com`;
 const resolverFunctions = {
+  //Main Search querys
   Query: {
-    hello: () => "Burak"
+    persons: () => {
+      return axios.get(`${mainUrl}/users`).then((res) => res.data);
+    },
+    person: (parent, { userId }) => {
+      return axios.get(`${mainUrl}/users/${userId}`).then((res) => res.data);
+    },
+    post: (parent, { postId }) => {
+      return axios.get(`${mainUrl}/posts/${postId}`);
+    }
+  },
+  //Nested Searchs
+  Person: {
+    posts: (parent) => {
+      return axios
+        .get(`${mainUrl}/posts?userId=${parent.id}`)
+        .then((res) => res.data);
+    }
+  },
+  Post: {
+    comments: (parent) => {
+      return axios
+        .get(`${mainUrl}/comments?postId=${parent.id}`)
+        .then((res) => res.data);
+    }
   }
 };
 
